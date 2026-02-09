70f6339f3671ebea96edd935d6c1b3e07e342330
LucasCamarero <l.camareroperez@ikasle.eus>
2026-02-09 15:12:25 +0100

    Commit inicial: Ejercicio sin empezar

---
+++ app/src/main/java/com/lucascamarero/pmd2_lucas/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.lucascamarero.pmd2_lucas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lucascamarero.pmd2_lucas.ui.theme.PMD2_lucasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PMD2_lucasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PMD2_lucasTheme {
+        Greeting("Android")
+    }
+}
